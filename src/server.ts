5c7e03ec5ba4f9068d4e04aed1d04dc70e4c81fe
itsmahmudul <islamashraf128@gmail.com>
2026-02-19 02:03:13 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
+const app = express();
+const port = 5000;
+
+//parser
+app.use(express.json());
+// app.use(express.urlencoded());
+
+//DB
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_fzs1rHlL7bOM@ep-flat-frost-aio7k0rb-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Next Level Developers!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    seccess: true,
+    message: "API is working",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
